7651e10b3a171b277e0697d0f5b2934d1fdbac32
alisonamerico <alison.americo@gmail.com>
2018-01-26 01:34:20 -0300

    0009-criando autenticação do usuário

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    path('', views.TodoListPageView.as_view(), name='todo_list'),
-
+    path('todo_list/', views.TodoListPageView.as_view(), name='todo_list'),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('registration/login/', views.LoginPageView.as_view(), name='login'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
-from django.views.generic import TemplateView, ListView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 
+from django.views.generic import TemplateView
 from . models import Todo
 
 
@@ -8,6 +11,17 @@ class HomePageView(TemplateView):
     template_name = 'home.html'
 
 
-class TodoListPageView(ListView):
+class TodoListPageView(TemplateView):
     model = Todo
     template_name = 'todo_list.html'
+
+
+class LoginPageView(TemplateView):
+    model = Todo
+    template_name = 'login.html'
+
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
