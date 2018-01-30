6292c66fb10b7e75f467088c50b34a1bf0554591
alisonamerico <alison.americo@gmail.com>
2018-01-30 20:34:07 -0300

    0022-atualizando models.py da app core

--- core/models.py
+++ core/models.py
@@ -1,15 +1,40 @@
 from django.db import models
-from datetime import datetime
-from django.urls import reverse
+
+import datetime
 
 
 class Todo(models.Model):
-    title = models.CharField(max_length=20)
-    text = models.TextField(max_length=30)
-    created_at = models.DateTimeField(default=datetime.now, blank=True)
+    title = models.CharField(max_length=250, unique=True)
 
     def __str__(self):
         return self.title
 
-    def get_absolute_url(self):
-        return reverse('todo_edit', kwargs={'pk': self.pk})
+    class Meta:
+        ordering = ['title']
+
+    class Admin:
+        pass
+
+
+PRIORITY_CHOICES = (
+  (1, 'Low'),
+  (2, 'Normal'),
+  (3, 'High'),
+)
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=250)
+    created_date = models.DateTimeField(default=datetime.datetime.now)
+    priority = models.IntegerField(choices=PRIORITY_CHOICES, default=2)
+    completed = models.BooleanField(default=False)
+    todo_list = models.ForeignKey('Todo', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-priority', 'title']
+
+    class Admin:
+        pass
