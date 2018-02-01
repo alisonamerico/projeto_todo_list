a75ad2437fde6c239e0da9ea1b162fca14385782
alisonamerico <alison.americo@gmail.com>
2018-02-01 02:06:31 -0300

    0030-ajustando os temples para realizar o CRUD

--- core/models.py
+++ core/models.py
@@ -1,19 +1,5 @@
 from django.db import models
-
 import datetime
-
-
-class Todo(models.Model):
-    title = models.CharField(max_length=250, unique=True)
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        ordering = ['title']
-
-    class Admin:
-        pass
 
 
 PRIORITY_CHOICES = (
@@ -23,18 +9,19 @@ PRIORITY_CHOICES = (
 )
 
 
-class Item(models.Model):
-    title = models.CharField(max_length=250)
+class Todo(models.Model):
+    title = models.CharField(max_length=250, unique=True)
     created_date = models.DateTimeField(default=datetime.datetime.now)
     priority = models.IntegerField(choices=PRIORITY_CHOICES, default=2)
     completed = models.BooleanField(default=False)
-    todo_list = models.ForeignKey('Todo', on_delete=models.PROTECT)
 
     def __str__(self):
         return self.title
 
     class Meta:
         ordering = ['-priority', 'title']
+        verbose_name = 'Todo'
+        verbose_name_plural = 'Todos'
 
     class Admin:
         pass
