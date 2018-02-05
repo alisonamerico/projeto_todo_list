a2fca80b8e13707d69ada9528cf36b3643750f57
alisonamerico <alison.americo@gmail.com>
2018-02-05 00:47:59 -0300

    0038-configurando reset de senha do usuario

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,12 +5,4 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
-    path('password_reset_form/', views.PasswordResetForm.as_view(), name='password_reset'),
-    path(
-        'password_reset_done/', views.PasswordResetForm.as_view(), name='password_reset_done'
-        ),
-    path(
-        'password_reset_confirm/',
-        views.PasswordResetConfirm.as_view(), name='password_reset_confirm'
-        ),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -7,15 +7,3 @@ class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
-
-
-class PasswordResetForm(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('password_reset_done')
-    template_name = 'password_reset_form.html'
-
-
-class PasswordResetConfirm(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('password_reset_confirm')
-    template_name = 'password_reset_done.html'
