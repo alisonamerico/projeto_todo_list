26332aab8295e0721f522a250bfb06148c75dc6f
alisonamerico <alison.americo@gmail.com>
2018-02-05 23:50:34 -0300

    0044-atualização do campo 'deadline'

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTests(SimpleTestCase):
+
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_page_contains_correct_html(self):
+        response = self.client.get('/')
+        self.assertContains(
+            response, '<h2>TODO LIST - APLICAÇÃO PARA REALIZAR LISTAGEM DE ATIVIDADES</h2>'
+            )
+
+    def test_home_page_does_not_contain_incorrect_html(self):
+        response = self.client.get('/')
+        self.assertNotContains(
+            response, 'Olá! Eu não deveria estar na página.')
