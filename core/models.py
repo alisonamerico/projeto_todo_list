0e7221f4f687ab92525e5a2f652a324f2192bf26
alisonamerico <alison.americo@gmail.com>
2018-02-06 02:42:13 -0300

    0051-alterando valor do deadline

--- core/models.py
+++ core/models.py
@@ -12,7 +12,7 @@ class Todo(models.Model):
 
     title = models.CharField(max_length=250, unique=True)
     created_date = models.DateTimeField(default=datetime.datetime.now)
-    deadline = models.DateTimeField(default=datetime.datetime.now)
+    deadline = models.DateTimeField(default=None)
     priority = models.CharField(max_length=6, choices=PRIORITY_CHOICES, default='Normal')
     completed = models.BooleanField(default=False)
 
